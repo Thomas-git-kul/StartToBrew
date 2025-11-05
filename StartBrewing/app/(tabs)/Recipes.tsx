294db1b31b9663b01b9c4f842dfd324873778981
Marie350 <marie.berx@student.kuleuven.be>
2025-11-05 15:25:50 +0100

    added react native paper search bar

--- StartBrewing/app/(tabs)/Recipes.tsx
+++ StartBrewing/app/(tabs)/Recipes.tsx
@@ -1,13 +1,14 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import { ThemedText } from "@/components/themed-text";
-import { StyleSheet, ScrollView, View } from "react-native";
+import React from "react";
+import { View, ScrollView } from "react-native";
+import { Text, Searchbar, IconButton } from "react-native-paper";
+import BeerCard from '@/components/ui/IPAcomponent';
 import { BASE_COLORS } from "@/constants/Colors";
 import { FontFamilies } from "@/constants/Fonts";
-import { useRouter } from "expo-router"; 
-import Checkbox from "expo-checkbox";
-import React, { useState } from "react";
-import BeerCard from '@/components/ui/IPAcomponent';
 
+import { SafeAreaView } from "react-native-safe-area-context";
+import { ThemedText } from "@/components/themed-text";
+import { StyleSheet} from "react-native";
+import { useRouter } from "expo-router"; 
 
 interface Beer {
   name: string;
@@ -18,8 +19,56 @@ interface Beer {
 }
 
 export default function Recipes() {
+  const [searchQuery, setSearchQuery] = React.useState("");
+
   const router = useRouter();
-  const beers: Beer[] = [
+
+  const popular_recipes: Beer[] = [
+    {
+      name: "IJ IPA",
+      rating: 4.8,
+      reviews: 256,
+      image: require("@/assets/images/default-beer.png"),
+      description: "An assertive bitterness that dominates the palate, with citrus and pine notes."
+    },
+    {
+      name: "Voodoo Ranger",
+      rating: 4.5,
+      reviews: 98,
+      image: require("@/assets/images/default-beer.png"),
+      description: "A crystal-clear IPA dominated by citrus and resin hop profile.",
+    },
+    {
+      name: "Two Hearted IPA",
+      rating: 4.9,
+      reviews: 322,
+      image: require("@/assets/images/default-beer.png"),
+      description: "A slightly hazy gold color with tropical flavors like mango and orange.",
+    },
+  ];
+
+  const all_recipes: Beer[] = [
+    {
+      name: "IJ IPA",
+      rating: 4.8,
+      reviews: 256,
+      image: require("@/assets/images/default-beer.png"),
+      description: "An assertive bitterness that dominates the palate, with citrus and pine notes."
+    },
+    {
+      name: "Voodoo Ranger",
+      rating: 4.5,
+      reviews: 98,
+      image: require("@/assets/images/default-beer.png"),
+      description: "A crystal-clear IPA dominated by citrus and resin hop profile.",
+    },
+    {
+      name: "Two Hearted IPA",
+      rating: 4.9,
+      reviews: 322,
+      image: require("@/assets/images/default-beer.png"),
+      description: "A slightly hazy gold color with tropical flavors like mango and orange.",
+    },
     {
       name: "IJ IPA",
       rating: 4.8,
@@ -44,18 +93,54 @@ export default function Recipes() {
   ];
 
   return (
-    <SafeAreaView style={styles.general}>
+    <View className="flex-1" 
+      style={{ 
+        backgroundColor: BASE_COLORS.LIGHT_BG 
+      }}
+    >
+    
+    {/* Fixed header */}
+    <View className="w-full z-50"
+      style={{
+        backgroundColor: BASE_COLORS.LIGHT_BG,
+      }}
+    >
+      <Text className=""
+        style={{
+          fontSize: 50,
+          color: BASE_COLORS.TEXT_DARK,
+          fontFamily: FontFamilies.HEADING,
+        }}
+      >Recipes</Text>
+
+      <Searchbar
+        placeholder="Search"
+        value={searchQuery}
+        onChangeText={setSearchQuery}
+        className="rounded-full"
+        icon="magnify"
+        style={{
+          backgroundColor: BASE_COLORS.WHITE,
+          borderWidth: 1,
+          borderColor: "#E7E5E4",
+        }}
+      />
+    </View>
+      
+      
+      
+      
+      
       <ScrollView>
-        <ThemedText style={styles.title}>Recipes</ThemedText>
         <ThemedText style={styles.title2}>Popular Recipes</ThemedText>
         <ThemedText style={styles.title2}>Recipes</ThemedText>
         <View style={{ paddingHorizontal: 10, paddingBottom: 20 }}>
-          {beers.map((beer, index) => (
+          {popular_recipes.map((beer, index) => (
             <BeerCard key={index} {...beer} />
           ))}
         </View>
       </ScrollView>
-    </SafeAreaView>
+    </View>
   );
 }
 
