bd283025dd847b4eba9f73171ccbab74c3bd2fba
thomas.monserez <monserez.thomas@gmail.com>
2025-11-22 23:13:11 +0100

    test

--- StartBrewing/app/(tabs)/Recipes.tsx
+++ StartBrewing/app/(tabs)/Recipes.tsx
@@ -137,7 +137,6 @@ export default function Recipes() {
       }}
     >
       <Header title="Recipes" />
-      <title>Recipes</title>
       {/* Searchbar */}
       <Searchbar
         placeholder="Search"
