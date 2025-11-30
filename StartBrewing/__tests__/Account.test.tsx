67ef94718fe1f335428657a656539a75e7a26e0a
Marie350 <marie.berx@student.kuleuven.be>
2025-11-30 23:04:04 +0100

    account test fix

--- StartBrewing/__tests__/Account.test.tsx
+++ StartBrewing/__tests__/Account.test.tsx
@@ -6,7 +6,7 @@ jest.mock("@react-native-async-storage/async-storage", () =>
 );
 
 jest.mock("@/hooks/use-fonts", () => ({
-  useFonts: () => {},
+  useFonts: () => true,
 }));
 
 jest.mock("@/hooks/beer-image", () => ({
