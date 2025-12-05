d6dabd7f924aae65dc537191433eb97fc2c48f2b
thomas.monserez <monserez.thomas@gmail.com>
2025-12-05 14:46:51 +0100

    test button 404

--- StartBrewing/app/Auth.tsx
+++ StartBrewing/app/Auth.tsx
@@ -145,6 +145,21 @@ export default function Auth() {
             }}
           >Log In</Button>
         </View>
+
+        <View className="mt-4">
+          <Button
+            mode="outlined"
+            onPress={() => router.push("/test")}
+            labelStyle={{
+              fontSize: Math.min(16 * scale, 24),
+              fontFamily: FontFamilies.BODY,
+              color: BASE_COLORS.TEXT_DARK,
+            }}
+            style={{
+              borderColor: BASE_COLORS.TEXT_DARK,
+            }}
+          >Test</Button>
+        </View>
       </View>
     </SafeAreaView>
   );
